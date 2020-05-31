a4984e2ac2ec129f08ca82d5ba1fa51d45d70a75
gonzoibox <gonzoibox@gmail.com>
2020-05-31 13:54:13 +0300

    use added service for croos-component commun-ion

--- app/src/app/recipes/recipe.service.ts
+++ app/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Test Recipe-1', 'Some Description-1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
         new Recipe('Test Recipe-2', 'Some Description-2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
@@ -9,4 +12,6 @@ export class RecipeService {
     getRecipes() {
         return this.recipes.slice();
     }
+
+
 }
