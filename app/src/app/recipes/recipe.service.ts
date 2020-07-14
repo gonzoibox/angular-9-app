187c2b291a02c08ea195ee29ec59bbe6784e736b
gonzoibox <gonzoibox@gmail.com>
2020-07-14 14:01:18 +0300

    transforming response data from Firebase

--- app/src/app/recipes/recipe.service.ts
+++ app/src/app/recipes/recipe.service.ts
@@ -10,18 +10,20 @@ export class RecipeService {
 
     constructor(private shoppingListService: ShoppingListService) {};
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Schnitzel',
-            'A super tasty Schnitzel',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
-            [new Ingredient('Meat', 1), new Ingredient('French Fries', 10)]),
-        new Recipe(
-            'Burger',
-            'An awesome Burger King',
-            'https://cdn.pixabay.com/photo/2015/04/20/13/25/burger-731298_1280.jpg',
-            [new Ingredient('Meat', 1), new Ingredient('Buns', 1)])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Schnitzel',
+    //         'A super tasty Schnitzel',
+    //         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+    //         [new Ingredient('Meat', 1), new Ingredient('French Fries', 10)]),
+    //     new Recipe(
+    //         'Burger',
+    //         'An awesome Burger King',
+    //         'https://cdn.pixabay.com/photo/2015/04/20/13/25/burger-731298_1280.jpg',
+    //         [new Ingredient('Meat', 1), new Ingredient('Buns', 1)])
+    //   ];
+
+    private recipes: Recipe[] = [];
     
     setRecipes(recipes: Recipe[]) {
         this.recipes = recipes;
--- app/src/app/shared/data-storage.service.ts
+++ app/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -21,7 +22,12 @@ export class DataStorageService {
     fetchRecipes() {
         this.http.get<Recipe[]>(
             'https://angular-recipe-book-464a2.firebaseio.com/recipes.json'
-        ).subscribe((response) => {
+        ).pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }))
+        .subscribe((response) => {
             this.recipeService.setRecipes(response);
         });
     }
