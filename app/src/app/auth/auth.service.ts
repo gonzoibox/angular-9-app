9758d34f25aba13d8320b3696588bcea13b38638
gonzoibox <gonzoibox@gmail.com>
2020-07-18 21:15:38 +0300

    add autologin function

--- app/src/app/auth/auth.service.ts
+++ app/src/app/auth/auth.service.ts
@@ -59,6 +59,27 @@ export class AuthService {
         }));
     }
 
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!!userData) {
+            const loadedUser = new User(
+                userData.email,
+                userData.id,
+                userData._token,
+                new Date(userData._tokenExpirationDate)
+            );
+
+            if (loadedUser.token) {
+                this.user.next(loadedUser);
+            }
+        } else return;
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -73,6 +94,7 @@ export class AuthService {
                 expirationDate
             );
             this.user.next(user);
+            localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
