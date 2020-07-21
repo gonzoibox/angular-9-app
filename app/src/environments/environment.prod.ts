67b716c561d6169861a5d553861ca9843838eeee
antergonzo <gonzoibox@gmail.com>
2020-07-21 16:16:37 +0300

    add API key

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyCOuYBhYS4_KfAWzihXGcH2cCone9YBN5g'
 };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyCOuYBhYS4_KfAWzihXGcH2cCone9YBN5g'
 };
 
 /*
